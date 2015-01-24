045a5cbdbb1cdbe4e95c9100ae46cece3da926cf
Ryan Slade <ryan@avocet.io>
2015-01-24 10:46:48 +0000

    Printing basic MemStats using termbox.

--- gotop.go
+++ gotop.go
@@ -6,8 +6,11 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"runtime"
 	"time"
+
+	tb "github.com/nsf/termbox-go"
 )
 
 type Info struct {
@@ -33,21 +36,81 @@ func httpGet(url string) (*Info, error) {
 	return parseJSON(rsp.Body)
 }
 
-func fetchLoop(interval time.Duration, url string) {
+func fetchLoop(interval time.Duration, url string, infoChan chan Info) {
 	ticker := time.NewTicker(interval)
 	for {
 		select {
 		case <-ticker.C:
-			info, err := httpGet(url)
+			i, err := httpGet(url)
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			fmt.Println(info.MemStats.Alloc)
+			infoChan <- *i
+		}
+	}
+}
+
+func draw(info Info) {
+	alloc := fmt.Sprintf("Alloc      : %d", info.MemStats.Alloc)
+	for i, r := range alloc {
+		tb.SetCell(i, 0, r, tb.ColorDefault, tb.ColorDefault)
+	}
+
+	heapAlloc := fmt.Sprintf("HeapAlloc  : %d", info.MemStats.HeapAlloc)
+	for i, r := range heapAlloc {
+		tb.SetCell(i, 1, r, tb.ColorDefault, tb.ColorDefault)
+	}
+
+	stackInUse := fmt.Sprintf("StackInUse : %d", info.MemStats.StackInuse)
+	for i, r := range stackInUse {
+		tb.SetCell(i, 2, r, tb.ColorDefault, tb.ColorDefault)
+	}
+}
+
+func drawLoop(interval time.Duration, infoChan chan Info) {
+	ticker := time.NewTicker(interval)
+	for {
+		select {
+		case <-ticker.C:
+			err := tb.Flush()
+			if err != nil {
+				log.Println(err)
+			}
+		case info := <-infoChan:
+			draw(info)
+		}
+	}
+}
+
+func eventLoop() {
+	for {
+		ev := tb.PollEvent()
+		switch ev.Type {
+		case tb.EventKey:
+			if ev.Key == tb.KeyEsc {
+				tb.Close()
+				os.Exit(0)
+			}
 		}
 	}
 }
 
 func main() {
-	fetchLoop(1*time.Second, "http://golang.org/debug/vars")
+	tb.Init()
+	defer tb.Close()
+
+	starting := "Starting..."
+	for i, r := range starting {
+		tb.SetCell(i, 0, r, tb.ColorDefault, tb.ColorDefault)
+	}
+	err := tb.Flush()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	infoChan := make(chan Info)
+	go fetchLoop(1*time.Second, "http://golang.org/debug/vars", infoChan)
+	go eventLoop()
+	drawLoop(1*time.Second, infoChan)
 }
